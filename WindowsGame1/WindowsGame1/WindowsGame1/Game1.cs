ebeb415ee5ca0e1853b08db0d164f1c017854871
Jason Yeomans <jyeo098@gmail.com>
2013-03-26 16:18:04 -0400

    Test Level & Collision

--- WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Game1.cs
@@ -18,14 +18,16 @@ namespace WindowsGame1
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D Dude,CrouchDude;
+        Texture2D Dude,CrouchDude,TestMap, WhiteTexture;
         Vector2 MySpritePosition = Vector2.Zero;
         bool W = false, A = false, S = false, D = false;
         string dir = "right";
-        int gravity = 2, jump = 50, surface = 385;
-        bool click = false;
-        bool canJump = true;
+        int gravity = 5, surface = 369, jump = 80;
+        bool canJump = true, jumping = false;
         MouseState m;
+        Rectangle dudeBox, basePlat,firstPlat,secPlat,thiPlat,fourPlat,fivePlat,sixPlat;
+        List<Rectangle> platList;
+        Color color = Color.White;
 
         int frameRate = 0;
         int frameCounter = 0;
@@ -47,8 +49,26 @@ namespace WindowsGame1
         {
             graphics.IsFullScreen = false;
             graphics.PreferredBackBufferHeight = 480;
-            graphics.PreferredBackBufferWidth = 854;
+            graphics.PreferredBackBufferWidth = 896;
             graphics.ApplyChanges();
+
+            dudeBox = new Rectangle((int)MySpritePosition.X,(int)MySpritePosition.Y,50,95);
+           //basePlat = new Rectangle(0, 464, 896, 16);
+            firstPlat = new Rectangle(192, 448, 64, 16);
+            secPlat = new Rectangle(256, 432, 64, 16);
+            thiPlat = new Rectangle(320, 416, 64, 16);
+            fourPlat = new Rectangle(384, 400, 48, 16);
+            fivePlat = new Rectangle(432, 384, 32, 16);
+            sixPlat = new Rectangle(480, 368, 32, 16);
+
+            platList = new List<Rectangle>();
+            //platList.Add(basePlat);
+            platList.Add(firstPlat);
+            platList.Add(secPlat);
+            platList.Add(thiPlat);
+            platList.Add(fourPlat);
+            platList.Add(fivePlat);
+            platList.Add(sixPlat);
 
             IsMouseVisible = true;
             // TODO: Add your initialization logic here
@@ -65,6 +85,8 @@ namespace WindowsGame1
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            WhiteTexture = Content.Load<Texture2D>("pixel");
+            TestMap = Content.Load<Texture2D>("TestMap");
             Dude = Content.Load<Texture2D>("dude");
             CrouchDude = Content.Load<Texture2D>("cdude");
             // TODO: use this.Content to load your game content here
@@ -89,7 +111,27 @@ namespace WindowsGame1
 
             m = Mouse.GetState();
 
-            
+            dudeBox.X = (int)MySpritePosition.X;
+            dudeBox.Y = (int)MySpritePosition.Y;
+
+            for (int i = 0; i < platList.Count; i++)
+            {
+                if (dudeBox.Intersects(platList[i]))
+                {
+                   
+                    Console.WriteLine("Intersected"+ i);
+                    surface = platList[i].Top - 95;
+                    jump = surface - 80;
+                    Console.WriteLine("Surface:1 " + surface);
+                }
+                else if(!dudeBox.Intersects(platList[i]))
+              {
+                    surface = 369;
+                }
+            }
+            Console.WriteLine("Surface 2: " + surface);
+
+
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
@@ -97,23 +139,37 @@ namespace WindowsGame1
             KeyboardState keyboard = Keyboard.GetState();
             if (keyboard.IsKeyDown(Keys.W) || (m.LeftButton == ButtonState.Pressed && (m.Y < MySpritePosition.Y - 110))) { W = true; /*MySpritePosition.Y -= 5;*/ }
             else { W = false; }
-            if (keyboard.IsKeyDown(Keys.S) || (m.LeftButton == ButtonState.Pressed && (m.Y > MySpritePosition.Y && (m.X > MySpritePosition.X && m.X<MySpritePosition.X+50)))) { S = true;  /*MySpritePosition.Y += 5;*/ }
+            if (keyboard.IsKeyDown(Keys.S) || (m.LeftButton == ButtonState.Pressed && (m.Y > MySpritePosition.Y && (m.X > MySpritePosition.X && m.X < MySpritePosition.X + 50)))) { surface = 369;  S = true;  /*MySpritePosition.Y += 5;*/ }
             else { S = false; }
             if (keyboard.IsKeyDown(Keys.A) || (m.LeftButton == ButtonState.Pressed && (m.X < MySpritePosition.X))) { dir = "left"; A = true; MySpritePosition.X -= 5; }
             else { A = false; }
             if (keyboard.IsKeyDown(Keys.D) || (m.LeftButton == ButtonState.Pressed && (m.X > MySpritePosition.X+50))) { dir = "right"; D = true; MySpritePosition.X += 5; }
             else { D = false; }
 
-            if (MySpritePosition.Y < surface)
+            if (W && canJump)
+            {
+                jumping = true;
+            }
+            if (jumping)
+                {
+                    if (MySpritePosition.Y > jump)
+                    {
+                        W = false;
+                        MySpritePosition.Y -= gravity + 5;
+                        canJump = false;
+                    }
+                    else
+                    { jumping = false; }
+                }
+            
+            else if (MySpritePosition.Y < surface)
             { MySpritePosition.Y += gravity; }
-            else
-            { if (MySpritePosition.Y == surface)
-                { canJump = true; } }
-            if (W) {
-                if (canJump) {
-                    W = false;
-                    MySpritePosition.Y -= jump;
-                    canJump = false; } }
+            if (MySpritePosition.Y >= surface)
+            {
+                
+                canJump = true; }
+
+
             base.Update(gameTime);
         }
 
@@ -124,7 +180,11 @@ namespace WindowsGame1
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
+
             spriteBatch.Begin();
+
+            spriteBatch.Draw(TestMap, new Vector2(0, 0), Color.White);
+
             if (dir.Equals("right") && S==false)
             { spriteBatch.Draw(Dude, MySpritePosition, Color.White); }
             else if (dir.Equals("left") && S==false)
